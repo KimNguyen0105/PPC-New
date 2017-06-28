503b1a7fc92d90b7c544b088ad904e7c6d2870cf
tinsqt <tinsqt@gmail.com>
2017-06-28 17:12:23 +0700

    update footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -3,34 +3,36 @@
     <hr style="border:2.5px solid #443427;width:100%; margin-left:0px;">
     <div class="text-center">
         <div class="col-md-3 col-sm-12">
-            <img src=" imgweb/5.gif" style="width:50%;">
+            <img src=" images/5.gif" style="width:50%;">
         </div>
+        @foreach($systems as $system)
         <div class="col-md-5 col-sm-12" style="text-align: left">
             <h4 style="text-align:left; color:#342315;"><b>Perfect Property Company VietNam</b></h4>
             <hr style="border:2.5px solid #342315;margin-left:0px;width:80px; margin-top: 5px" id="hr">
-            <p style="text-align: left;font-family: verdana;font-weight: 300; font-size: 12pt; color:#342315;">{diachicongty}</p>
+            <p style="text-align: left;font-family: verdana;font-weight: 300; font-size: 12pt; color:#342315;">{{$system->company_info}}</p>
             <p style="text-align: left;font-family: verdana;font-weight: 300; font-size: 12pt; color:#342315;">Phone: +84 8 38201107</p>
             <p style="text-align: left;font-family: verdana;font-weight: 300; font-size: 12pt; color:#342315;">Hotline: 0988 084 009</p>
         </div>
-        <div class="col-md-4 col-sm-12" style="text-align: left">
-            <h4 style="text-align:left; color:#342315;"><b>Perfect Property Company USA</b></h4>
-            <hr style="border:2.5px solid #342315;margin-left:0px;width:80px; margin-top: 5px" id="hr">
-            <p style="text-align: left;font-family: verdana;font-weight: 300; font-size: 12pt; color:#342315;">42 Broadway Suit 12-233 New York, NY 10004</p>
-            <p style="text-align: left;font-family: verdana;font-weight: 300; font-size: 12pt; color:#342315;">Phone: 917 831 0562</p>
-            <p style="text-align: left;font-family: verdana;font-weight: 300; font-size: 12pt; color:#342315;">Email: info@perfectproperties.vn</p>
-        </div>
+        @endforeach
+        {{--<div class="col-md-4 col-sm-12" style="text-align: left">--}}
+            {{--<h4 style="text-align:left; color:#342315;"><b>Perfect Property Company USA</b></h4>--}}
+            {{--<hr style="border:2.5px solid #342315;margin-left:0px;width:80px; margin-top: 5px" id="hr">--}}
+            {{--<p style="text-align: left;font-family: verdana;font-weight: 300; font-size: 12pt; color:#342315;">42 Broadway Suit 12-233 New York, NY 10004</p>--}}
+            {{--<p style="text-align: left;font-family: verdana;font-weight: 300; font-size: 12pt; color:#342315;">Phone: 917 831 0562</p>--}}
+            {{--<p style="text-align: left;font-family: verdana;font-weight: 300; font-size: 12pt; color:#342315;">Email: info@perfectproperties.vn</p>--}}
+        {{--</div>--}}
     </div>
 </div>
 
 <div class="container">
-    <hr style="border:2.5px solid #342315;margin-left:0px;">
+    <hr style="border:2.5px solid #342315;margin-left:0px;">d
 
     <div class="iconlink text-center">
         <ul style="list-style-type:none;">
-            <li style="display:inline;"><a href="#"><img src=" imgweb/social1.png"></a></li>
-            <li style="display:inline;"><a href="#"><img src=" imgweb/social2.png"></a></li>
-            <li style="display:inline;"><a href="#"><img src=" imgweb/social3.png"></a></li>
-            <li style="display:inline;"><a href="#"><img src=" imgweb/social4.png"></a></li>
+            <li style="display:inline;"><a href="#"><img src=" images/social1.png"></a></li>
+            <li style="display:inline;"><a href="#"><img src=" images/social2.png"></a></li>
+            <li style="display:inline;"><a href="#"><img src=" images/social3.png"></a></li>
+            <li style="display:inline;"><a href="#"><img src=" images/social4.png"></a></li>
         </ul>
     </div>
     <div class="text-center" style="padding: 0 15px;">
