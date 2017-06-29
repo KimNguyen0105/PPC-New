1e65809864396761a050511f75fe944d91f1fd6f
KimNguyen <thuykim0105@gmail.com>
2017-06-29 17:09:28 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -31,22 +31,60 @@ Route::get('/500.html','Controller@badinternal');
 |--------------------------------------------------------------------------
 | Admin Routes
 |--------------------------------------------------------------------------*/
+//admin
 Route::get('admin/log-in','UserController@GetLogIn');
 Route::post('admin/log-in','UserController@PostLogIn');
 Route::get('admin/log-out','UserController@LogOut');
 Route::group(['prefix'=>'admin'],function (){
     Route::get('/home', 'UserController@Home');
-
+    //slider
     Route::get('/slide', 'SlideController@Home');
     Route::post('/slide-save', 'SlideController@SaveSlide');
     Route::get('/slide-xoa/{id}', 'SlideController@XoaSlide');
-
+//Video home
     Route::get('/video-home', 'VideoHomeController@Home');
     Route::post('/video-home-save', 'VideoHomeController@SaveVideo');
     Route::get('/video-home-xoa/{id}', 'VideoHomeController@XoaVideo');
-
+//introduce
     Route::get('/introduce-home', 'IntroduceController@Home');
     Route::get('/introduce/{id}', 'IntroduceController@GetIntroduce');
     Route::post('/introduce-save', 'IntroduceController@SaveIntroduce');
+//category news
+    Route::get('/category-home', 'NewsController@CategoryHome');
+    Route::post('/category-save', 'NewsController@CategorySave');
+//news
+    Route::get('/news-home', 'NewsController@NewsHome');
+    Route::get('/news/{id}', 'NewsController@GetNews');
+    Route::get('/news-delete/{id}', 'NewsController@DeleteNews');
+    Route::post('/news-save', 'NewsController@SaveNews');
+//gallery news
+    Route::get('/news-gallery-home', 'NewsController@NewsGalleryHome');
+    Route::get('/news-gallery/{id}', 'NewsController@GetNewsGallery');
+    Route::post('/news-gallery-save', 'NewsController@SaveNewsGallery');
+//gallery image news
+    Route::get('/news-gallery-image/{id}', 'NewsController@NewsGalleryImageHome');
+    Route::get('/news-gallery-image-delete/{idnews}-{id}', 'NewsController@DeleteGalleryImage');
+    Route::post('/news-gallery-image-save','NewsController@SaveNewsGalleryImage');
+//gallery video
+    Route::get('/gallery-video', 'NewsController@GalleryVideoHome');
+    Route::get('/gallery-video-delete/{id}', 'NewsController@DeleteGalleryVideo');
+    Route::post('/gallery-video-save','NewsController@SaveGalleryVideo');
+    //recruitment
+    Route::get('/recruitment', 'RecruitmentController@RecruitmentHome');
+    Route::get('/recruitment/{id}', 'RecruitmentController@GetRecruitment');
+    Route::get('/recruitment-delete/{id}', 'RecruitmentController@DeleteRecruitment');
+    Route::post('/recruitment-save','RecruitmentController@SaveRecruitment');
 
+    //term
+    Route::get('/term', 'TermController@Home');
+    Route::get('/term/{id}', 'TermController@GetTerm');
+    Route::post('/term-save','TermController@SaveTerm');
+//partners
+    Route::get('/partners', 'TermController@PartnersHome');
+    Route::post('/partners-save','TermController@SavePartners');
+    Route::get('/partners-delete/{id}','TermController@DeletePartners');
+
+    //system config
+    Route::get('/system-config', 'ConfigController@Home');
+    Route::post('/config-save', 'ConfigController@SaveConfig');
 });
