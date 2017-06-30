6744fc3d810b79656ac607804f4e8ea990c1c8fb
tinsqt <tinsqt@gmail.com>
2017-06-30 13:16:19 +0700

    update Page/hrpolicies file

--- resources/views/Page/hrpolicies.blade.php
+++ resources/views/Page/hrpolicies.blade.php
@@ -4,22 +4,23 @@
     <div class="backgroundbody">
         <div class="container paddingchitiet">
             <div class="text-center">
-                <h3 class="titlewweb" style="color:#443427;"><b>{GIOITHIEU}</b></h3>
+                <h3 class="titlewweb" style="color:#443427;"><b>{{trans('home.hr_policies')}}</b></h3>
                 <hr style="border:2.5px solid #443427;width:80px;">
 
             </div>
 
             <div class="space">
-                <?php
 
-                foreach ($data as $row)
-                { ?>
+
+                @foreach ($data as $row)
+
                 <div class="col-md-4 col-sm-12 text-center divgioithieu">
-                    <a href=""><img src="images/{{$row->image}}" class="img-responsive img_body" style=" width: 100%"></a>
-                    <a href=""><h4 class="titlewweb" style="color:#443427;"><b>{!! $row->title !!}</b></h4></a>
+                    <a href="{{URL::asset('')}}hr-policies-detail/{{$row->id}}-{{$row->slug}}.html">
+                        <img src="{{URL::asset('')}}images/{{$row->image}}" class="img-responsive img_body" style=" width: 100%"></a>
+                    <a href="">
+                        <h4 class="titlewweb" style="color:#443427;"><b>{!! $row->title !!}</b></h4></a>
                 </div>
-                <?php }
-                ?>
+                @endforeach
             </div>
         </div>
     </div>
