9150450a782e2da45f36e0566a213114b6205223
KimNguyen <thuykim0105@gmail.com>
2017-07-03 16:36:37 +0700

    add Property_lang

---
+++ app/Property_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Property_lang extends Model
+{
+    //
+    protected $table='property_lang';
+    public $timestamps = false;
+}
