cd7e0b11524ed218c8798da340b7220a65bab728
KimNguyen <thuykim0105@gmail.com>
2017-07-03 16:36:16 +0700

    add Property_Image

---
+++ app/Property_Image.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Property_Image extends Model
+{
+    //
+    protected $table='property_image';
+    public $timestamps = false;
+}
