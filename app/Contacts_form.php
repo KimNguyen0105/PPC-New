9429f7710c7c8928c9e816c67d03baf1b1fb5998
KimNguyen <thuykim0105@gmail.com>
2017-07-05 08:56:49 +0700

    add model

---
+++ app/Contacts_form.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contacts_form extends Model
+{
+    //
+    protected $table='contacts_form';
+    public $timestamps = true;
+}
